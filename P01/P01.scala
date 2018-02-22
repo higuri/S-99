ffd6c602bb079c3774ca063a11decbc0675e8b40
higuri <35358263+higuri@users.noreply.github.com>
2018-02-22 23:37:15 +0900

    Change last() to use generic function

--- P01/P01.scala
+++ P01/P01.scala
@@ -1,11 +1,15 @@
+/// P01
 object P01 {
   // last:
-  def last(list0: List[Int]): Int = {
+  // - generic (polymorphic) method [parameterized by type].
+  def last[A](list0: List[A]): A = {
     return list0.last
   }
 
   // main:
   def main(args: Array[String]): Unit = {
+    //println(last[Int](List(1, 1, 2, 3, 5, 8)))
+    // type inference available!
     println(last(List(1, 1, 2, 3, 5, 8)))
   }
 }
