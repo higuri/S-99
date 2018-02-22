8c9f384805a6ab6294e26e1b7e1c45745bd6c301
higuri <35358263+higuri@users.noreply.github.com>
2018-02-23 00:51:18 +0900

    Change last() to use list pattern matching

--- P01/P01.scala
+++ P01/P01.scala
@@ -1,11 +1,17 @@
 /// P01
 object P01 {
   // last:
-  // - generic (polymorphic) method [parameterized by type].
+  // * generic (polymorphic) method [parameterized by type].
   def last[A](list0: List[A]): A = {
+    // use list pattern match.
+    // * list is built from
+    //   elem0 :: elem1 :: elem2 :: Nil
+    //   [Nil terminated].
     list0 match {
-      case h :: Nil => h
-      case _ :: tail => last(tail)
+      // e.g. 8 :: Nil
+      case head :: Nil => head
+      // e.g. 1 :: List(1, 2, 3, 5, 8)
+      case head :: list1 => last(list1)
       case _         => throw new Exception
     }
   }
