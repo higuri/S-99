ae2811992e56db42e9aa595317b54bf400e24427
higuri <35358263+higuri@users.noreply.github.com>
2018-02-22 23:16:20 +0900

    Add P01.scala

---
+++ P01/P01.scala
@@ -0,0 +1,11 @@
+object P01 {
+  // last:
+  def last(list0: List[Int]): Int = {
+    return list0.last
+  }
+
+  // main:
+  def main(args: Array[String]): Unit = {
+    println(last(List(1, 1, 2, 3, 5, 8)))
+  }
+}
